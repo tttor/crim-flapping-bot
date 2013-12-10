ceec7bda8af10d12a0a02ff586419b0ec481f884
reaper1412 <andi.antonius@gmail.com>
2013-12-10 11:44:27 +0700

    Basic camera capture function using openCV

--- gnd-ctrl/camera/DisplayImage.cpp
+++ gnd-ctrl/camera/DisplayImage.cpp
@@ -1,25 +1,25 @@
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
-//#include <opencv/highgui.h>
+#include "opencv2/opencv.hpp"
 
 using namespace cv;
 
-int main( int argc, char** argv )
+int main(int, char**)
 {
-  Mat image;
-  image = imread( argv[1], 1 );
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
 
-  if( argc != 2 || !image.data )
+    //Mat edges;
+    namedWindow("edges",1);
+    for(;;)
     {
-      printf( "No image data \n" );
-      return -1;
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        //cvtColor(frame, edges, CV_BGR2GRAY);
+        //GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        //Canny(edges, edges, 0, 30, 3);
+        imshow("edges", frame);
+        if(waitKey(30) >= 0) break;
     }
-
-  namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
-  imshow( "Display Image", image );
-
-  waitKey(0);
-
-  return 0;
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
 }
-

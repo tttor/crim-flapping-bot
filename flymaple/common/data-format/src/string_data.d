2181b6524fc472bde6662f0a45d8c0e0718ec96d
tor <vektor.dewanto@gmail.com>
2013-12-10 10:05:19 +0700

    remove /flymaple/common and untracked files under that dir

--- flymaple/common/data-format/src/string_data.d
+++
@@ -1,5 +0,0 @@
-build/../common/data-format/include/../src/string_data.o: \
- /home/tor/robotics/prj/008/crim-flapping-bot/common/data-format/src/string_data.cpp \
- ../common/data-format/include/data-format/string_data.h
-
-../common/data-format/include/data-format/string_data.h:
--- flymaple/common/packet/src/packet.d
+++
@@ -1,5 +0,0 @@
-build/../common/packet/include/../src/packet.o: \
- /home/tor/robotics/prj/008/crim-flapping-bot/common/packet/src/packet.cpp \
- ../common/packet/include/packet/packet.h
-
-../common/packet/include/packet/packet.h:

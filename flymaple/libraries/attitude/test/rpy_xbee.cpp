d3878d37d10c673d70273d48c8d0367cb04f3864
Vektor Dewanto <vektor.dewanto@gmail.com>
2013-12-30 11:57:47 +0700

    another test script: rpy via xbee

---
+++ flymaple/libraries/attitude/test/rpy_xbee.cpp
@@ -0,0 +1,49 @@
+#include "wirish.h"
+#include "attitude/attitude.h"
+#include "xbee/flymaple_packet_handler.h"
+#include "data-format/pose_data.h"
+
+// Force init() to be called before anything else.
+__attribute__((constructor)) void premain() {
+	init();
+}   
+
+int main(void) {
+  using namespace crim;
+  
+  Attitude attitude;
+  FlymaplePacketHandler packer_handler("Serial3", 9600);
+  
+  while(1) {
+    SerialUSB.println("looping");
+
+    attitude.read();
+    
+    double x, y, z;
+    x = y = z = 0.0;
+
+    double roll, pitch, yaw;
+    roll = attitude.roll();
+    pitch = attitude.pitch();
+    yaw = attitude.yaw();
+  
+    SerialUSB.print("roll (rad) = "); SerialUSB.print(roll); SerialUSB.print("\t");
+    SerialUSB.print("pitch (rad) = "); SerialUSB.print(pitch); SerialUSB.print("\t");
+    SerialUSB.print("yaw (rad) = "); SerialUSB.print(yaw); SerialUSB.print("\t");
+    SerialUSB.println();  
+  
+    PoseData data;
+    data.set_parent_frame("world");
+    data.set_frame("flymaple");
+    data.set_origin(x, y, z);
+    data.set_rotation(roll, pitch, yaw);
+    
+    packer_handler.wrap(data);
+    packer_handler.send();
+    
+    delay(500);
+  }
+  
+  return 0;
+  
+}

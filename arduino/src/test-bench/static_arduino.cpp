cf12002e8e5af9df11c6ce0293560d16e5fcb488
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-02-18 11:38:17 +0700

    initial test-bench setting

--- arduino/src/test-bench/static_arduino.cpp
+++ arduino/src/test-bench/static_arduino.cpp
@@ -2,10 +2,23 @@
 #include <comm/arduino_mavlink_packet_handler.hpp>
 #include <macro/macro.h>
 
+struct TestBenchPacket {
+  mavlink_attitude_t att_msg;
+  mavlink_global_position_int_t pos_msg;
+  mavlink_rc_channels_raw_t rc_msg;
+};
+
 int main() {
   init();// this needs to be called before setup() or some functions won't work there
   Serial.begin(115200);
   
+  // Setup the static encoder
+  const size_t encoder_out_a_pin = 2;
+  const size_t encoder_out_b_pin = 3;
+  const uint64_t encoder_resolution = 360;
+  crim::TwoPhaseIncrementalEncoder encoder(encoder_out_a_pin, encoder_out_b_pin, encoder_resolution);
+  
+  // Setup the mavlink packet handler
   mavlink_system_t mavlink_system;
   mavlink_system.sysid = MAV_TYPE_TEST_BENCH;
   mavlink_system.compid = MAV_COMP_ID_STATIC_TEST_BENCH_ARDUINO;

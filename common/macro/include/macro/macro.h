12cc42972109adc6adbf450aa474ce5e6d48abb1
Vektor Dewanto <vektor.dewanto@gmail.com>
2014-02-18 10:01:13 +0700

    add kPhi

--- common/macro/include/macro/macro.h
+++ common/macro/include/macro/macro.h
@@ -2,9 +2,13 @@
 #define MACRO_H
 
 namespace crim {
-  enum angleType {DEG, RAD};
-  static const double kPI = 3.1415926;
-  
+
+enum angleType {DEG, RAD};
+const double kPI = 3.1415926;
+const double kPhi = kPI;
+const double kOneRadianInDegree = 57.2957795;
+const double kOneDegreeInRadian = 0.0174532925;
+
 } // namespace crim
 
 #endif
